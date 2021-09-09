b2eafee3a247c3048a56838aed39b3a36ca70a44
agustinbianchi <agustinbianchi2107@gmail.com>
2021-09-09 15:00:36 -0300

    agregando index app y navbar js

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import Nav from "./NavBar"
+
+const App = () => {
+    return (
+        <header>
+            <h1>MOROBAT</h1>
+            <Nav/>
+        </header>
+    )
+}
+
+export default App
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import style from "./style.css"
+
+const Nav = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#">INICIO</a></li>
+                <li><a href="#">PRODUCTOS</a></li>
+                <li><a href="#">NOSOTROS</a></li>
+                <li><a href="#">CONTACTANOS</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
